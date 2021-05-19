9fc03d5c28f9c87310b541c112f36951161cec80
David Gaseni <david.gaseni@estudiants.urv.cat>
2021-05-19 20:50:21 +0100

    Lithops test downloading object-storage files

--- lithops_test.py
+++ lithops_test.py
@@ -1,4 +1,22 @@
 from lithops import Storage
+import json
+import time
+import lithops
+from lithops.multiprocessing import Pool
 
-storage = Storage()
-storage.put_object('tweepy', 'test.txt', 'Hello World')
+def f(data):
+    review = json.loads(data)
+    return(review['reviewText'])
+
+def readData(dic):
+    storage = Storage()
+    data = storage.get_object('analysis.data', 'rawdata/Automotive_5.json').decode()
+    data = data.replace("}{","}\n{").splitlines()
+    with Pool() as pool:
+        result = pool.map(f, data)
+    return result
+    
+if __name__ == '__main__':
+    with lithops.FunctionExecutor() as fexec:
+        fexec.call_async(readData, None)
+        print(fexec.get_result())
